f44b43d910bafd2c4c94f8836688d0b66e69c7c6
James Andersen <james.andersen@universalmind.com>
2014-08-27 23:03:04 -0600

    Working on some rough layout and styling for student schedules using test data

--- src/scripts/app/WelcomeCtrl.js
+++ src/scripts/app/WelcomeCtrl.js
@@ -1,3 +1,4 @@
+/*global angular: false */
 (function () {
     'use strict';
     angular.module('myApp').controller('WelcomeCtrl', ['$scope', '$log', 'hsAuthService', 'hsCalendarService',
@@ -14,5 +15,73 @@
                     $log.error(error);
                 });
             };
+
+            $scope.setActiveStudent = function (activeStudent) {
+                angular.forEach($scope.students, function (student) { student.active = student === activeStudent; });
+                $scope.activeStudent = activeStudent;
+            };
+
+            $scope.students = [
+                {
+                    name: null,
+                    nonStudent: true,
+                    events: [
+                        {
+                            time: 510, // 8:30 * 60,
+                            assignment: "8:30"
+                        },
+                        {
+                            time: 570, // 9:30 * 60,
+                            assignment: "9:30"
+                        }
+                    ]
+                },
+                {
+                    name: "Ella",
+                    active: true,
+                    events: [
+                        {
+                            time: 510, // 8:30 * 60,
+                            subject: "History",
+                            assignment: "Read pg. 34"
+                        },
+                        {
+                            time: 570, // 8:30 * 60,
+                            subject: "Math",
+                            assignment: "Do Worksheet 7"
+                        }
+                    ]
+                },
+                {
+                    name: "Nina",
+                    events: [
+                        {
+                            time: 510, // 8:30 * 60,
+                            subject: "History",
+                            assignment: "Read pg. 34"
+                        },
+                        {
+                            time: 570, // 8:30 * 60,
+                            subject: "Math",
+                            assignment: "Do Worksheet 7"
+                        }
+                    ]
+                },
+                {
+                    name: "Phoebe",
+                    events: [
+                        {
+                            time: 510, // 8:30 * 60,
+                            subject: "History",
+                            assignment: "Read pg. 34"
+                        },
+                        {
+                            time: 570, // 8:30 * 60,
+                            subject: "Math",
+                            assignment: "Do Worksheet 7"
+                        }
+                    ]
+                }
+            ];
         }]);
 }());
